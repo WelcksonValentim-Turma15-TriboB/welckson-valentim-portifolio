b3cc9099326bb15c2b498d98d0b0c65cac91e2fc
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-02-09 19:58:07 -0300

    feat: Contact.js, cria caminho para portifolio do gitHyb

--- src/Pages/Contatc.js
+++ src/Pages/Contatc.js
@@ -1,7 +1,23 @@
+import Header from "../Components/Header";
+import GitHub_Logo_White from '../images/GitHub_Logo_White.png';
+
 function Contact() {
     return (
       <section>
+        <Header />
         <h1>Contatos</h1>
+        <a href='//www.linkedin.com/in/welcksonvalentim' target="_blank" rel="noreferrer noopener" >
+          <img 
+            alt='logo Linkedin' 
+            src='https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white'
+          />
+        </a>
+        <a href='https://github.com/WelcksonValentim-Turma15-TriboB' target="_blank" rel="noreferrer noopener" >
+          <img 
+            alt='logo GitHub' 
+            src={ GitHub_Logo_White }
+          />
+        </a>
       </section>
     );
   }
