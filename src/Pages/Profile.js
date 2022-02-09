76587d5b04e804d568390746949f19dad1be0b9b
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-02-09 13:34:16 -0300

    feat: Profile.js, adiciona titulo e texto

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,8 +1,14 @@
 function Profile() {
   return (
-    <div>
-      <h1>test</h1>
-    </div>
+    <section>
+      <h1>Quem sou eu</h1>
+      <p>Engenheiro mecânico com mais de 5 anos de atuação,
+        decidi investir em um sonho antigo de atuar com tecnologia.
+        Possuo experiência com gestão, gerência de projetos, planeamento,
+        metodologia ágil e desenvolvi habilidades técnicas em Front End,
+        inciando no momento o desenvolvimento em Back End (estudando na Trybe).
+      </p>
+    </section>
   );
 }
 
