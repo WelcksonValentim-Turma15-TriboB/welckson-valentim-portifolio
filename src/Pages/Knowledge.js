2389fb6a09553a23980d6ef169046dc9989708bd
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-02-11 19:13:14 -0300

    style: APP.js, finaliza estilizaçao do componentes de conhecimento

--- src/Pages/Knowledge.js
+++ src/Pages/Knowledge.js
@@ -10,34 +10,40 @@ function Knowledge() {
       <section className='knowledge'>
       <h1>Conhecimentos</h1>
         <h3>Plataformas</h3>
-        <ul>
-          <li>Visual Studio code</li>
-          <li>Git</li>
-          <li>GitHub</li>
-          <li>MySQL Workbench</li>
-        </ul>
-        <img alt='logo Visual Studio code' src={ Visual_Studio_Code_icon } />
+        <section className='plat-knowledge'>
+          <ul>
+            <li>Visual Studio code</li>
+            <li>Git</li>
+            <li>GitHub</li>
+            <li>MySQL Workbench</li>
+          </ul>
+          <img alt='logo Visual Studio code' src={ Visual_Studio_Code_icon } />
+        </section>
       </section>
       <section className='knowledge'>
         <h3>Tecnologias - Front-End</h3>
-        <ul>
-          <li>React</li>
-          <li>React Testing library</li>
-          <li>JavaScript</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Bootstrap</li>
-        </ul>
-        <img alt='logo HTML_CSS_JS_Bootstrap' src={ HTML_CSS_JS_Bootstrap } />
+        <section className='plat-knowledge'>
+          <ul>
+            <li>React</li>
+            <li>React Testing library</li>
+            <li>JavaScript</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Bootstrap</li>
+          </ul>
+          <img alt='logo HTML_CSS_JS_Bootstrap' src={ HTML_CSS_JS_Bootstrap } />
+        </section>
       </section>
       <section className='knowledge'>
         <h3>Tecnologias - Back-End</h3>
-        <ul>
-          <li>Docker</li>
-          <li>SQL</li>
-          <li>Node.JS</li>
-        </ul>
-        <img alt='logo nodejs_MYSQL_docker' src={ nodejs_MYSQL_docker } />
+        <section className='plat-knowledge'>
+          <ul>
+            <li>Docker</li>
+            <li>SQL</li>
+            <li>Node.JS</li>
+          </ul>
+          <img alt='logo nodejs_MYSQL_docker' src={ nodejs_MYSQL_docker } />
+        </section>
       </section>
     </main>
   );
