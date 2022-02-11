1e429690401afc9c3a5796cf1e22d23a06d2f1cf
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-02-11 16:37:58 -0300

    style: APP.js, estiliza componente de conhecimentos e inseri imagens sobre as tecnologias

--- src/Pages/Contatc.js
+++ src/Pages/Contatc.js
@@ -3,27 +3,27 @@ import social_github_icon from '../images/social_github_icon.png';
 import linkedin_logo_icon from '../images/linkedin_logo_icon.png';
 import office365_outlook_icon from '../images/office365_outlook_icon.png';
 import instagram_logo_icon from '../images/instagram_logo_icon.png';
-import Footer from '../Components/Footer';
 
 function Contact() {
     return (
-      <section>
+      <main>
         <Header />
-        <h1>Contatos</h1>
-        <a href='//www.linkedin.com/in/welcksonvalentim' target="_blank" rel="noreferrer noopener">
-          <img alt='logo Linkedin' src={ linkedin_logo_icon } />
-        </a>
-        <a href='https://github.com/WelcksonValentim-Turma15-TriboB' target="_blank" rel="noreferrer noopener">
-          <img alt='logo GitHub' src={ social_github_icon } />
-        </a>
-        <a href='mailto:wellvalentim@hotmail.com?'>
-          <img alt='logo Email' src={ office365_outlook_icon } />
-        </a>
-        <a href='hhtps://instagram.com/welcksonvalentim' target='_blank' rel='noreferrer noopener'>
-          <img alt='logo Instagram' src={ instagram_logo_icon } />
-        </a>
-        <Footer />
-      </section>
+        <section className='contact'>
+          <h1>Contatos</h1>
+          <a href='//www.linkedin.com/in/welcksonvalentim' target="_blank" rel="noreferrer noopener">
+            <img alt='logo Linkedin' src={ linkedin_logo_icon } />
+          </a>
+          <a href='https://github.com/WelcksonValentim-Turma15-TriboB' target="_blank" rel="noreferrer noopener">
+            <img alt='logo GitHub' src={ social_github_icon } />
+          </a>
+          <a href='mailto:wellvalentim@hotmail.com?'>
+            <img alt='logo Email' src={ office365_outlook_icon } />
+          </a>
+          <a href='hhtps://instagram.com/welcksonvalentim' target='_blank' rel='noreferrer noopener'>
+            <img alt='logo Instagram' src={ instagram_logo_icon } />
+          </a>
+        </section>
+      </ main>
     );
   }
   
--- src/Pages/Knowledge.js
+++ src/Pages/Knowledge.js
@@ -1,11 +1,14 @@
-import Header from "../Components/Header";
+import Header from '../Components/Header';
+import Visual_Studio_Code_icon from '../images/Visual_Studio_Code_icon.png';
+import HTML_CSS_JS_Bootstrap from '../images/HTML_CSS_JS_Bootstrap.png';
+import nodejs_MYSQL_docker from '../images/nodejs_MYSQL_docker.png';
 
 function Knowledge() {
   return (
     <main>
       <Header />
+      <section className='knowledge'>
       <h1>Conhecimentos</h1>
-      <section>
         <h3>Plataformas</h3>
         <ul>
           <li>Visual Studio code</li>
@@ -13,8 +16,9 @@ function Knowledge() {
           <li>GitHub</li>
           <li>MySQL Workbench</li>
         </ul>
+        <img alt='logo Visual Studio code' src={ Visual_Studio_Code_icon } />
       </section>
-      <section>
+      <section className='knowledge'>
         <h3>Tecnologias - Front-End</h3>
         <ul>
           <li>React</li>
@@ -24,14 +28,16 @@ function Knowledge() {
           <li>CSS</li>
           <li>Bootstrap</li>
         </ul>
+        <img alt='logo HTML_CSS_JS_Bootstrap' src={ HTML_CSS_JS_Bootstrap } />
       </section>
-      <section>
+      <section className='knowledge'>
         <h3>Tecnologias - Back-End</h3>
         <ul>
           <li>Docker</li>
           <li>SQL</li>
           <li>Node.JS</li>
         </ul>
+        <img alt='logo nodejs_MYSQL_docker' src={ nodejs_MYSQL_docker } />
       </section>
     </main>
   );
