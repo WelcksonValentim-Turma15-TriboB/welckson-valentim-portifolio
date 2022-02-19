109958a4d6d5999950c01dde7bc55328511937c3
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-02-19 10:37:48 -0300

    welckson valim - sytle: app.css, atualiza estilizaçao condicional para mobile frist

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -8,11 +8,10 @@ function Profile() {
       <Header />
       <section className='profile'>
       <h1>Quem sou eu ?</h1>
-        <p>Sou engenheiro mecânico com mais de 5 anos de atuação,
-          decidi investir em um sonho antigo de atuar com tecnologia.
-          Possuo experiência com gestão, gerência de projetos, planeamento,
-          metodologia ágil e desenvolvi habilidades técnicas em Front End,
-          inciando no momento o desenvolvimento em Back End (estudando na Trybe).
+        <p>Formado em engenharia mecânica, atuei mais de 5 anos na área.
+          Atualmente estou estudando na Trybe iniciando o módulo de desenvolvimento em 
+          Back End (Docker | SQL | Nodejs). Possuo experiência com gestão, gerência de projetos, 
+          planejamento, metodologia ágil e Front End (React | React Testing Library | JavaScript).
         </p>
         <img width='250px' alt='Welckson photography' src={ photo_welckson } />
         <h4>Welckson Valentim</ h4>
