53d97bb87ab01b985653e8d098ff651c667a8f8b
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-03-18 09:53:41 -0300

    welckson valentim - style: background, modifica plano de fundo dos componentes

--- src/Pages/Knowledge.js
+++ src/Pages/Knowledge.js
@@ -8,41 +8,43 @@ function Knowledge() {
   return (
     <main>
       <Header />
-      <h1>Conhecimentos</h1>
-      <section className='knowledge'>
-        <section className='plat-knowledge'>
-        <h3>Softwares</h3>
-          <ul>
-            <li>Visual Studio Code</li>
-            <li>Git</li>
-            <li>GitHub</li>
-            <li>MySQL Workbench</li>
-          </ul>
-          <img alt='logo softwares' src={ softwares } />
-        </section>
-        <section className='plat-knowledge'>
-        <h3>Tecnologias - Front-End</h3>
-          <ul>
-            <li>React</li>
-            <li>React Testing library</li>
-            <li>JavaScript</li>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>Bootstrap</li>
-          </ul>
-          <img alt='logo frontEnd' src={ frontEnd } />
-        </section>
-        <section className='plat-knowledge'>
-        <h3>Tecnologias - Back-End</h3>
-          <ul>
-            <li>Docker</li>
-            <li>SQL</li>
-            <li>Node.JS</li>
-            <li>TypeScript</li>
-          </ul>
-          <img alt='logo backEnd' src={ backEnd } />
-        </section>
-        </section>
+      <section className='knowleg'>
+        <h1>Conhecimentos</h1>
+        <section className='knowledge'>
+          <section className='plat-knowledge'>
+          <h3>Softwares</h3>
+            <ul>
+              <li>Visual Studio Code</li>
+              <li>Git</li>
+              <li>GitHub</li>
+              <li>MySQL Workbench</li>
+            </ul>
+            <img alt='logo softwares' src={ softwares } />
+          </section>
+          <section className='plat-knowledge'>
+          <h3>Tecnologias - Front-End</h3>
+            <ul>
+              <li>React</li>
+              <li>React Testing library</li>
+              <li>JavaScript</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Bootstrap</li>
+            </ul>
+            <img alt='logo frontEnd' src={ frontEnd } />
+          </section>
+          <section className='plat-knowledge'>
+          <h3>Tecnologias - Back-End</h3>
+            <ul>
+              <li>Docker</li>
+              <li>SQL</li>
+              <li>Node.JS</li>
+              <li>TypeScript</li>
+            </ul>
+            <img alt='logo backEnd' src={ backEnd } />
+          </section>
+          </section>
+      </section>
       <Footer />
     </main>
   );
