8a8853f9c8538d95ccbb244c93521833fa7e640e
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-04-06 10:12:32 -0300

    welckson valentim - style: estiliza componente projetcs

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -7,28 +7,35 @@ function Projects() {
       <Header />
       <section className='projects'>
         <h1>Projetos</h1>
-        <a href='https://listofcarsfrontend.herokuapp.com/cars' target="_blank" rel="noreferrer noopener">
-          <h4>Aplicação list Of Cars</h4>
-          <iframe 
-            title='gif project listOfCars'
-            src="https://giphy.com/embed/RehiJfEYcpIQlOAc2c"
-            width="480"
-            height="270"
-            frameBorder="0"
-            class="giphy-embed" 
-            allowFullScreen
-          >
-            </iframe>
-          <p>Com esta aplicação podemos visualizar lista de veículos, com descrição de marca, modelo e versão.
-            Ao clicar sobre o card abre-se uma nova página, do site WebMotors, com as especificações técnicas de cada modelo.</p>
-          <ul>
-            <li>Tecnologias – Front end: React.js, Javascript, HTML, CSS;</li>
-            <li>Tecnologias – Back end: Node.js, Sequelize, MySQL, Cors, Docker;</li>
-            <li>Plataformas - Supabase e Heroku;</li>
-            <li>Link repositório – Front End: https://github.com/welcksonvalentim/listofcarsfrontend</li>
-            <li>Link repositório – Back End: https://github.com/welcksonvalentim/listOfCarsBackEnd</li>
-          </ul>
-        </a>
+          <div className='divProjects'>
+            <h2>Aplicação list Of Cars</h2>
+            <a
+              href='https://listofcarsfrontend.herokuapp.com/cars'
+              target="_blank"
+              rel="noreferrer noopener"
+              className='linkProjects'
+            >
+              <iframe 
+                title='gif project listOfCars'
+                src="https://giphy.com/embed/RehiJfEYcpIQlOAc2c"
+                frameBorder="0"
+              >
+              </iframe>
+              <p>Com esta aplicação podemos visualizar lista de veículos, com descrição de marca, modelo e versão.
+                Ao clicar sobre o card abre-se uma nova página, do site WebMotors, com as especificações técnicas de cada modelo.</p>
+              <ul>
+                <li>Tecnologias – Front end: React.js, Javascript, HTML, CSS;</li>
+                <li>Tecnologias – Back end: Node.js, Sequelize, MySQL, Cors, Docker;</li>
+                <li>Plataformas – Supabase e Heroku;</li>
+              </ul>
+             </a>
+            <a href='https://github.com/welcksonvalentim/listofcarsfrontend' target="_blank" rel="noreferrer noopener">
+              <h4>Repositório - Front End: https://github.com/welcksonvalentim/listofcarsfrontend</h4>
+            </a>
+            <a href='https://github.com/welcksonvalentim/listOfCarsBackEnd' target="_blank" rel="noreferrer noopener">
+              <h4>Repositório - Back End: https://github.com/welcksonvalentim/listOfCarsBackEnd</h4>
+            </a>
+          </div>
       </section>
       <Footer />
     </main>
