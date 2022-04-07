7e5fe921e1254affd53a24bca0af1f50be29037a
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-04-07 08:51:15 -0300

    welckson valentim - style: modifica formatação do componente contact

--- src/Pages/Contatc.js
+++ src/Pages/Contatc.js
@@ -12,15 +12,19 @@ function Contact() {
         <section className='contact'>
           <h1>Contatos</h1>
           <a href='//www.linkedin.com/in/welcksonvalentim' target="_blank" rel="noreferrer noopener">
+            <h4>Linkedin</h4>
             <img alt='logo Linkedin' src={ linkedin_logo_icon } />
           </a>
           <a href='https://github.com/welcksonvalentim' target="_blank" rel="noreferrer noopener">
+            <h4>GitHub</h4>
             <img alt='logo GitHub' src={ social_github_icon } />
           </a>
           <a href='mailto:wellvalentim@hotmail.com?'>
+            <h4>Outlook</h4>
             <img alt='logo Email' src={ office365_outlook_icon } />
           </a>
           <a href='hhtps://instagram.com/welcksonvalentim' target='_blank' rel='noreferrer noopener'>
+            <h4>Instagram</h4>
             <img alt='logo Instagram' src={ instagram_logo_icon } />
           </a>
         </section>
